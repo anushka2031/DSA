92fc328cf9653306424c9a8bdc59841af592f605
anushka2031 <anushkatiwari2031@gmail.com>
2026-03-18 12:28:53 +0530

    18 Mar

--- linkedList.java
+++ linkedList.java
@@ -33,6 +33,63 @@ public class linkedList {
 
     }
 
+    void reverse(){
+
+        if(head == null || head.next == null){
+            return;
+        }
+
+        Node curr = head;
+        Node temp;
+
+        while (curr.next!=null) {
+            temp = curr.next;
+            curr.next = temp.next;
+            temp.next = head;
+            head = temp;
+        }
+        
+    }
+
+    void mid(){
+
+       Node slow = head;
+       Node fast = head;
+       while(slow.next!= null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+       }
+       System.out.println(slow.data);
+    }
+
+    void cycle(){
+
+       Node slow = head;
+       Node fast = head;
+       while(fast!= null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(fast == slow){
+            System.out.println("Has Cycle");
+        }
+       }
+       System.out.println("No Cycle");
+
+    }
+
+    // void deleteEnd(){
+
+    //     while ((head == null || head.next == null)) {
+    //         head = null;
+    //         return;
+    //     }
+
+    //     temp = head;
+    //     while (temp.next.next != null) {
+    //         temp.next=null;
+    //     }
+    // }
+
     void display(){
         temp = head ;
         
@@ -49,7 +106,12 @@ public class linkedList {
         list.insert(1);
         list.addNodes(2);
         list.addNodes(3);
+        list.addNodes(4);
+        // list.deleteEnd();
+        // list.reverse();
         list.display();
+        // list.mid();
+        list.cycle();
 
       
 
